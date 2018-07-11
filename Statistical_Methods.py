76bd79a739828aa3c532d7377ccd059d22c0ce9d
Soojung <hong.soojung@gmail.com>
2018-07-11 18:04:10 +0200

    Box-Cox power transformation

--- Statistical_Methods.py
+++ Statistical_Methods.py
@@ -4,5 +4,27 @@
 Created on Wed Jul 11 13:36:30 2018
 
 @author: soojunghong
+
+@about : experiment to evaluate the model using metrics 
 """
 
+#------------------------------------
+# Box-Cox Power transformation
+#------------------------------------
+from scipy import stats
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+x = stats.loggamma.rvs(5, size=500) + 5
+prob = stats.probplot(x, dist=stats.norm, plot=ax1)
+ax1.set_xlabel('')
+ax1.set_title('Probplot against normal distribution')
+
+# use box cox 
+ax2 = fig.add_subplot(212)
+xt, _ = stats.boxcox(x)
+prob = stats.probplot(xt, dist=stats.norm, plot=ax2)
+ax2.set_title('Probplot after Box-Cox transformation')
+
+plt.show()
