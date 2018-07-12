2eaa5d154ff5cb8ae248eab6e24e3c74b419d235
Soojung <hong.soojung@gmail.com>
2018-07-12 13:28:07 +0200

    statistics in python

--- Statistical_Methods.py
+++ Statistical_Methods.py
@@ -84,3 +84,64 @@ from scipy.stats import ttest_ind
 cat1 = my_data[my_data['Category'] == 'cat1']
 cat2 = my_data[my_data['Category'] == 'cat2']
 ttest_ind(cat1['values'], cat2['values']) # ttest_ind : Calculate the T-test for the means of two independent samples of scores.
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
+# scipy.stats.ttest_ind() is to calculate the T-test for the means of Two independent samples of scores
+# 
+# with real world example. 
+# null-hypothesis : there is no statistically significant difference in the mean of male consulting doctor and junior resident female doctors
+#---------------------------------------------------------------------------------------------------------------------------------------------
+female_doctor_bps = [128, 127, 118, 115, 144, 142, 133, 140, 132, 131, 
+                     111, 132, 149, 122, 139, 119, 136, 129, 126, 128]
+
+male_consultant_bps = [118, 115, 112, 120, 124, 130, 123, 110, 120, 121,
+                      123, 125, 129, 130, 112, 117, 119, 120, 123, 128]
+
+stats.ttest_ind(female_doctor_bps, male_consultant_bps)
+#p-value is less than 0.05, we reject the null hypothesis, means that we can say there is statistically signigicant difference betwwen two groups 
+
+
+#------------------------------------------------------------------------------------------------------------
+# ANOVA test - used to compare the means of three or more samples
+# ANOVA will provide an F-statistics which can, along with degrees of freedom, be used to calculate p-value
+#-------------------------------------------------------------------------------------------------------------
+ctrl = [4.17, 5.58, 5.18, 6.11, 4.5, 4.61, 5.17, 4.53, 5.33, 5.14]
+trt1 = [4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69]
+trt2 = [6.31, 5.12, 5.54, 5.5, 5.37, 5.29, 4.92, 6.15, 5.8, 5.26]
+
+stats.f_oneway(ctrl, trt1, trt2)
+
+
+#---------------------------------------
+# Data representation and interaction
+#---------------------------------------
+import pandas
+data = pandas.read_csv('/Users/soojunghong/Documents/safariML/Statistical_Method/brain_size.csv', sep = ';', na_values = ".")
+data
+
+import numpy as np
+t = np.linspace(-6, 6, 20) #from -6 until 6, 20 numbers 
+t
+sin_t = np.sin(t)
+cos_t = np.cos(t)
+
+pandas.DataFrame({'t':t, 'sin':sin_t, 'cos':cos_t})
+
+data.shape #(40, 8) - 40 rows and 8 columns 
+data.columns
+pandas.DataFrame.describe(data) #it shows count, mean, std, min 25%, 50%, 75%
+print(data['Gender'])
+
+#groupby - splitting a dataframe on values on categorical variables 
+groupby_gender = data.groupby('Gender')
+groupby_gender
+for gender, value in groupby_gender['VIQ']:
+    print((gender, value.mean()))
+
+groupby_gender.mean()  
+
+# plotting data
+from pandas.tools import plotting
+plotting.scatter_matrix(data[['Weight', 'Height', 'MRI_Count']])  
